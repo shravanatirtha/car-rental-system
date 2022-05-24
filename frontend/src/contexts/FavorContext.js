2ac54e4c19912227ca2a15c8f40c1e67800e343d
shravanatirtha <shravz.ceo@gmail.com>
2022-05-24 20:49:40 +0530

    context theming

--- frontend/src/contexts/FavorContext.js
+++ frontend/src/contexts/FavorContext.js
@@ -28,7 +28,7 @@ const FavorProvider = ({ children }) => {
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
-      fill="blueviolet"
+      fill="darkslateblue"
       viewBox="0 0 24 24"
     >
       <path d="M12 4.248c-3.148-5.402-12-3.825-12 2.944 0 4.661 5.571 9.427 12 15.808 6.43-6.381 12-11.147 12-15.808 0-6.792-8.875-8.306-12-2.944z" />
@@ -49,7 +49,7 @@ const FavorProvider = ({ children }) => {
   const myCartSvg = (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      fill="blueviolet"
+      fill="darkslateblue"
       width="24"
       height="24"
       viewBox="0 0 24 24"
