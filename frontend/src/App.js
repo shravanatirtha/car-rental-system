8ec81ff03bd1d17e208bf1bd42299ba44230380c
shravanatirtha <shravz.ceo@gmail.com>
2022-05-24 14:12:55 +0530

    added basic files

---
+++ frontend/src/App.js
@@ -0,0 +1,42 @@
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Signup from "./pages/Auth/Signup";
+import Signin from "./pages/Auth/Signin";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import Profile from "./pages/Profile";
+import ProtectedRoute from "./pages/ProtectedRoute";
+import Basket from "./pages/Basket";
+import Error404 from "./pages/Error404";
+import Admin from "./pages/Admin";
+import Contact from "./pages/Contact";
+import Favor from "./pages/Favor";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+
+        <div id="content">
+          <Switch>
+            <Route path="/" exact component={Products} />
+            <Route path="/product/:product_id" component={ProductDetail} />
+            <Route path="/signin" component={Signin} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/basket" component={Basket} />
+            <Route path="/favor" component={Favor} />
+
+            <ProtectedRoute path="/profile" component={Profile} />
+            <ProtectedRoute path="/admin" component={Admin} admin={true} />
+            <Route path="*" component={Error404} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
